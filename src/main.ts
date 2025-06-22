fac255ee0e738586840d14e1c42214dc6f17f10c
Johann Patrício Daniel <johannpatriciod@gmail.com>
2025-06-21 22:50:05 -0300

    aprendendo com o projeto

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Falha ao iniciar a aplicação:', err);
+  process.exit(1); // Encerra com código de erro
+});
